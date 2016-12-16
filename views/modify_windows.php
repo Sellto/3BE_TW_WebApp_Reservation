bf6a3c60d7364b7b4ab582fe09a5dfc043727bf9
sellto <selleslagh.tom@gmail.com>
2016-12-16 09:44:27 +0100

    Fix modify windows style

--- views/modify_windows.php
+++ views/modify_windows.php
@@ -1,12 +1,10 @@
-<div class="Container2">
-<div id=<?php echo "modify".$dbreserv->GetID()?> class=windows>
+<div id=<?php echo "modify".$dbreserv->GetID()?> class="windows container">
   <div>
 
         <?php include("controllers/controller_modify.php");?>
         <div class="btn-group">
-          <button type="submit" name="Modify" class="btn btn-success" form=<?php echo "myform".$dbreserv->GetID(); ?> formaction="index.php?page=manager" value=<?php echo $reserv->GetID()?>>Save</button>
-          <button type="submit" name="Modify" class="btn btn-danger" form=<?php echo "myform".$dbreserv->GetID(); ?> formaction="index.php?page=manager" value="Abort" >Abort</button>
+          <button type="submit" name="Modify" class="btn btn-default" form=<?php echo "myform".$dbreserv->GetID(); ?> formaction="index.php?page=manager" value=<?php echo $reserv->GetID()?>><span class="glyphicon glyphicon-floppy-disk"></span></button>
+          <button type="submit" name="Modify" class="btn btn-default" form=<?php echo "myform".$dbreserv->GetID(); ?> formaction="index.php?page=manager" value="Abort" ><span class="glyphicon glyphicon-remove-circle"></span></button>
         </div>
 	</div>
-</div>
 </div>
