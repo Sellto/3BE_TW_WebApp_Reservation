c411a80404e843b2ba4ed9aad1f6e4964b83b7d9
sellto <selleslagh.tom@gmail.com>
2016-12-16 09:50:13 +0100

    Fix delete windows style

--- views/delete_windows.php
+++ views/delete_windows.php
@@ -1,9 +1,9 @@
-<div id=<?php echo "Delete".$dbreserv->GetID()?> class="windows">
+<div id=<?php echo "Delete".$dbreserv->GetID()?> class="windows container">
   <div>
-		<h2>Are You Sure?</h2>
+		<h4>Êtes-vous sûre de vouloir supprimer cette reservation?</h4>
       <form method="POST" action="index.php?page=manager">
-          <button type="submit" class="btn btn-success" name="Delete" value=<?php echo $dbreserv->GetID()?>>Yes</button>
-          <button type="submit" class="btn btn-danger" value="No ">No</button>
+          <button type="submit" class="btn btn-default" name="Delete" value=<?php echo $dbreserv->GetID()?>><span class="glyphicon glyphicon-ok-circle"></button>
+          <button type="submit" class="btn btn-default" value="No "><span class="glyphicon glyphicon-remove-circle"></span></button>
       </form>
 	</div>
 </div>
