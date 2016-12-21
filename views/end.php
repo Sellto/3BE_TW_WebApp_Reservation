0df6d1b5c98082e0a0efb2c980535cc41dc4b289
sellto <selleslagh.tom@gmail.com>
2016-12-21 09:40:44 +0100

    Update Thanks Page

--- views/end.php
+++ views/end.php
@@ -1,2 +1,2 @@
-thanks
+<div class="container"><h4>Merci pour votre réservation, nous vous souhaitons d'or et déja un très bon voyage.</h4></div>
 <form method="POST" action="index.php" id="myform"></form>
